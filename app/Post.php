aec446483257c230336d4d8478ccc34dd0b38fc8
Vuong Hoang <vuonghv.cs@gmail.com>
2017-11-16 11:14:59 +0700

    Create and show comments

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body) {
+        $this->comments()->create(compact('body'));
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,14 +3,23 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{ $post->title }}</div>
+            <div class="col-md-12">
+                <h3>{{ $post->title }}</h3>
+                <small>{{ $post->created_at->toFormattedDateString() }}</small>
+                <p>{{ $post->body }}</p>
+                <hr/>
+                @foreach( $post->comments as $comment )
+                    <p>{{ $comment->body }}<small class="text-muted">&nbsp;&nbsp;{{ $comment->created_at->diffForHumans() }}</small></p>
+                @endforeach
 
-                    <div class="panel-body">
-                        {{ $post->body }}
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" class="form-control" id="exampleInputEmail1" placeholder="Input your comment" required></textarea>
                     </div>
-                </div>
+                    @include('layouts.error')
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
             </div>
         </div>
     </div>

b2cc8b3d1ee4fbfced9a099eb715fa4258d3d748
Vuong Hoang <vuonghv.cs@gmail.com>
2017-11-17 09:07:54 +0700

    Add user relationship to posts/comments

--- app/Comment.php
+++ app/Comment.php
@@ -11,4 +11,9 @@ class Comment extends Model
     public function post() {
         return $this->belongsTo(Post::class);
     }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,10 @@ class Post extends Model
 {
     protected $fillable = ['title', 'body'];
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
     public function comments() {
         return $this->hasMany(Comment::class);
     }
